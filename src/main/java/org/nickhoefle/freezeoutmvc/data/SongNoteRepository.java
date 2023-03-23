6786a2ffe0f3863d7f17523410f0274e1d115021
Nick <nickhoefle@gmail.com>
2023-03-22 22:39:19 -0500

    added missing @Repository annotations for SongNoteRepository/UserRepository

--- src/main/java/org/nickhoefle/freezeoutmvc/data/SongNoteRepository.java
+++ src/main/java/org/nickhoefle/freezeoutmvc/data/SongNoteRepository.java
@@ -2,6 +2,8 @@ package org.nickhoefle.freezeoutmvc.data;
 
 import org.nickhoefle.freezeoutmvc.models.SongNote;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SongNoteRepository extends CrudRepository<SongNote, Integer> {
 }
