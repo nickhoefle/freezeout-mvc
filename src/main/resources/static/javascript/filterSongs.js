932d985cc6d33d11deb1bf50e8d1b4b9a6e29dea
Nick Hoefle <Nickhoefle@gmail.com>
2023-12-19 20:25:13 -0600

    Added placeholder to Delete Songs filter input; added functionality to filter by artist.

--- src/main/resources/static/javascript/filterSongs.js
+++ src/main/resources/static/javascript/filterSongs.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     function filterTable() {
-        let input, filter, table, tr, td, i, txtValue;
+        let input, filter, table, tr, td, i, txtValueSong, txtValueArtist;
         input = document.getElementById("filterInput");
         filter = input.value.toUpperCase();
         table = document.getElementById("myTable");
         tr = table.getElementsByTagName("tr");
 
         for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tdSong = tr[i].getElementsByTagName("td")[1];
+            tdArtist = tr[i].getElementsByTagName("td")[2];
+            if (tdSong && tdArtist) {
+                txtValueSong = tdSong.textContent || tdSong.innerText;
+                txtValueArtist = tdArtist.textContent || tdArtist.innerText;
+                if (
+                    txtValueSong.toUpperCase().indexOf(filter) > -1 ||
+                    txtValueArtist.toUpperCase().indexOf(filter) > -1
+                ) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
@@ -22,4 +27,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("filterInput").addEventListener("input", filterTable);
 
-})
+});
